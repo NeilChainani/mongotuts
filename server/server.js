953b24a8ce2a136073d98d54c1aee1d78744fc5e
Neil Chainani <Neil.Chainani@lexisnexisrisk.com>
2018-12-21 14:31:18 -0500

    switch to express

--- server/server.js
+++ server/server.js
@@ -1,45 +1,26 @@
-var mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
-var Todo = mongoose.model("Todo", {
-  text: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-/*var newTodo = new Todo({ text: "Cook dinner" });
-newTodo.save().then(
-  doc => {
-    console.log("Saved Todo", doc);
-  },
-  e => {
-    console.log("unable to save todo");
-  }
-);*/
-var otherTodo = new Todo({
-  text: true,
-  completed: true,
-  completedAt: 1
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });
-otherTodo.save().then(a => {
-  console.log(a);
-});
-
-//user email
-//email  require/trim//string min 1
-//new users collection
-/*var db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error"));
-db.once("open", function() {});
-*/
